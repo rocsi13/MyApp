29629f230ba669656e515d64a4a7c0df4576eff8
rocsi13 <103496696+rocsi13@users.noreply.github.com>
2023-10-02 15:32:17 +0300

    Add files via upload

---
+++ src/main/java/myApp/myApp/Entity/User.java
@@ -0,0 +1,46 @@
+package myApp.myApp.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private String fullName;
+    @Column(nullable=false, unique=true)
+    private String email;
+    @Column(nullable=false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.MERGE)
+    @JoinTable(
+            name="users_roles",
+            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
+            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
+    private List<Role> roles = new ArrayList<>();
+
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String fullName, String email, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+    }
+}
---
+++ src/main/java/myApp/myApp/Service/UserService.java
@@ -0,0 +1,14 @@
+package myApp.myApp.Service;
+
+import myApp.myApp.Entity.User;
+import myApp.myApp.Entity.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+    void saveUser(UserDto userDto);
+
+    User findByEmail(String email);
+
+    List<UserDto> findAllUsers();
+}
