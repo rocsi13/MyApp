5ab7148fd069eb37a03d9e061b31297fb34b5f3c
rocsi13 <103496696+rocsi13@users.noreply.github.com>
2023-10-17 13:26:22 +0300

    Update CustomUserDetailsService.java

--- src/main/java/myApp/myApp/Service/CustomUserDetailsService.java
+++ src/main/java/myApp/myApp/Service/CustomUserDetailsService.java
@@ -25,32 +25,18 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        System.out.println("Email/loadUserbyEmail/CustomUSerDetailsService");
-        System.out.println(email);
-        System.out.println("USer Repository/load User by Username/CustomUSerDetailsService");
-        System.out.println(userRepository);
         User user = userRepository.findByEmail(email);
-        System.out.println("user/CustomUSerDetailsService");
-        System.out.println(user);
         if (user != null) {
-            System.out.println("Load User Not Null/CustomUSerDetailsService");
-            System.out.println(user);
-            return new CustomUserDetails(user.getEmail(), user.getPassword(), authorities(user.getRoles()), user.getFullName());
+            return new CustomUserDetails(user.getEmail(), user.getPassword(), authorities(user.getRoles()), user.getFullName(), user.getVoted());
         }else{
-            System.out.println("Else user/password invalid/CustomUSerDetailsService");
-            System.out.println(user);
             throw new UsernameNotFoundException("Invalid username or password.");
         }
     }
 
     public Collection<? extends GrantedAuthority> authorities(Collection<Role> roles){
-        System.out.println("GrantedAuthority");
         Collection < ? extends GrantedAuthority> mapRoles = roles.stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toList());
-        System.out.println(mapRoles);
         return mapRoles;
     }
-
-
 }
